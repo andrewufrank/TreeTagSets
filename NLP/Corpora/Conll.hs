258411f0037b90bc0301c7b2eabfe025bd32987c
Andrew Frank <frank@geoinfo.tuwien.ac.at>
2018-03-04 23:55:56 +0100

    added undef for conll - should be done for others as well

--- NLP/Corpora/Conll.hs
+++ NLP/Corpora/Conll.hs
@@ -23,6 +23,7 @@ import  NLP.Types.Tags  (POStags (..), TagsetIDs (..)
 --import NLP.Types.General
 import Data.Utilities
 
+undefConll = error "convertOneSnip2Triples postag conll":: POStag
 ---- | Named entity categories defined for the Conll 2003 task.
 --data NERtag = PER
 --            | ORG
